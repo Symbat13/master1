7ffd2e1cdd4ffaeb6e7810b4a81462601801793b
Bibizada <user@Lenova-PC.(none)>
2017-10-05 14:10:26 +0400

    Ushinshi commit

--- mytest1/kz/kaznitu/lessons/Hello.java
+++ mytest1/kz/kaznitu/lessons/Hello.java
@@ -5,5 +5,10 @@ public class Hello {
         System.out.println("Hello World!");
         System.out.println("Bul zerthanalyk zhumystyn maksaty:");
         System.out.println("GitHub-pen zhumys zhasap uirenu.");
+
+        int a= MyMath.kvadrat(a:5);
+        int b=MyMath.kub(a:5);
+        System.out.println("a= " +a);
+        System.out.println("b= " +b);
     }
 }
